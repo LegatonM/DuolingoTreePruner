951ee29e987ac65c6eb42ab8d60b15913af129c3
Legato <mikael.enarssen.sp15@thegameassembly.com>
2017-07-10 15:51:35 +0200

    Trying to export button html and style to separate files.

--- DuoTreeTrimmer/DuoTreeTrimmer.user.js
+++ DuoTreeTrimmer/DuoTreeTrimmer.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Duolingo Tree Pruner
 // @namespace    http://tampermonkey.net/
-// @version      1.2.0
+// @version      1.2.1
 // @description  Tree pruner to work with the new version of Duolingo's website
 // @author       Legato né Mikael
 // @match        https://www.duolingo.com/*
@@ -13,8 +13,6 @@
 
 // This is an updated version of the tree trimmer by Arek Olek (https://www.duolingo.com/comment/12962386), intended to work with the current version of Duoling (2017-07-10). Credit for the original idea goes to Thomas de Roo
 
-var trimmerStyle = '';
-var trimmerButton = '<a id="trimmerButton" class="_3LN9C _3QG2_ _1vaUe _3IS_q _1XnsG _1vaUe _3IS_q" style="margin-left: 0.5em; padding-top: .75em; padding-buttom: .75em; " style="display: block"></a>';
 var maxHealth = 5;
 
 //In the new version of the site, I could not find an "easy" way to find out what strength a lesson has. These classes are associated with strength in the current version.
@@ -23,7 +21,7 @@ var strengthClasses = ["_22Kj3", "_13qn4", "rgwrb", "_2SEng", "dYBTa"];
 function UpdateButtonText()
 {
     var trimValue = GM_getValue("trimValue",maxHealth);
-    
+
     $('*[data-test="skill-tree"] .mAsUf #trimmerButton').html("<span>" + (trimValue < maxHealth ? trimValue + " bars or less" : "Everything") + "</span>");
 }
 
@@ -59,12 +57,20 @@ $('body').on("click", "#trimmerButton", function(){
     PruneTree();
 });
 
+function CreateButton()
+{
+    $.get("", function(data){
+        console.log(data);
+        //$('*[data-test="skill-tree"] .mAsUf').prepend(trimmerButton);
+    }, "text");
+}
+
 function Init()
 {
     setTimeout(function(){
         if($('*[data-test="skill-tree"] .mAsUf #trimmerButton').size() === 0)
         {
-            $('*[data-test="skill-tree"] .mAsUf').prepend(trimmerButton);
+            CreateButton();
         }
         UpdateButtonText();
         PruneTree();
